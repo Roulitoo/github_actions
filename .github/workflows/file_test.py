bcd1a963ec8da343708d6eae1641c9f9607fe39f
roulito <roul.benjamin@gmail.com>
2023-09-05 21:22:40 +0000

    new

--- .github/workflows/file_test.py
+++ .github/workflows/file_test.py
@@ -14,8 +14,3 @@ def test_calc_multiply():
   # Fonction test du résultat de 2*4
     output = 2*4
     assert output == 8
-
-def test_coucou():
-  # Fonction test si la résultat renvoie 'hello'
-    output='hello'
-    assert output == 'hello'
